7d0b52f623896ce72cbc848ef333b16b9a0fe449
Craig Christopher Messi <craig.messi@gmail.com>
2020-03-09 09:18:54 +0000

    Automatically backed up by Learn

--- lib/bonus.rb
+++ lib/bonus.rb
@@ -21,10 +21,8 @@ def bonus
   }
 
   #code your solution here:
-  epic_tragedy[:montague] = fam1
-  fam1[:hero][:status] = "dead"
-  epic_tragedy[:capulet] = fam2
-  fam2[:heroine][:status] = "dead"
+  epic_tragedy[:capulet][:heroine][:status] = "dead"
+  epic_tragedy[:montague][:hero][:status] = "dead"
   
 
   #Don't touch the following line! The `bonus` method must return our newly modified epic tragedy hash
